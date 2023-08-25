ccc8414f7a1f635f9937a98c9c25d5f1c6748aa3
vishnaka-dasanayaka <vishnakadasanayaka@gmail.com>
2023-08-25 11:10:34 +0530

    home page course renderring

--- Frontend/src/components/Courses/Courses.jsx
+++ Frontend/src/components/Courses/Courses.jsx
@@ -1,62 +1,59 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getAllCourses, reset } from "../../features/courses/courseSlice";
+import Spinner from "../Spinner/Spinner";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 function Courses() {
+  const dispatch = useDispatch();
+  const { allCourses, isLoading } = useSelector((state) => state.courses);
+
+  useEffect(() => {
+    dispatch(getAllCourses());
+
+    return () => {
+      dispatch(reset());
+    };
+  }, [dispatch]);
+
+  if (isLoading) {
+    return <Spinner />;
+  }
+
   return (
-    <div>
-      
-      <div>
-        <h1 className='ml-[20px] text-5xl '>Courses on Edu.com</h1>
+    <div className="m-w-[1200px]">
+      <h1 className="ml-[20px] text-5xl">Courses on Edu.com</h1>
+
+      <div className="w-screen overflow-x-auto whitespace-nowrap">
+        {allCourses.length > 0 ? (
+          <div className="inline-flex m-5">
+            {allCourses.map((course) => (
+              <div
+                key={course.id}
+                className="min-w-[300px] mr-[20px] bg-[#d2d2d2] rounded-xl p-[20px]"
+              >
+                <div className="rounded-xl m-5 bg-[#d2d2d2]">
+                  <div className="backImage bg-center bg-cover rounded-[50%] mt-10 mx-auto w-[170px] h-[170px]"></div>
+
+                  <div className="flex flex-col items-center justify-center">
+                    <h2 className="text-xl mt-10 py-1 px-5 text-white rounded-lg bg-[#373737]">
+                      {course.subject}
+                    </h2>
+                    <h3 className="mt-5">teacher name</h3>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        ) : (
+          <h1 className="empty-message">
+            oops ..! there are no any courses in our site by now
+          </h1>
+        )}
       </div>
-      <div className='grid grid-cols-4 h-[400px]'>
-
-        <div className='rounded-xl m-5 bg-[#d2d2d2]'>
-            <div
-                className="backImage bg-center bg-cover rounded-[50%] mt-10 mx-auto w-[170px] h-[170px]">
-            </div>
-
-            <div className='flex flex-col items-center justify-center'>
-                <h2 className='text-xl mt-10 py-1 px-5 text-white rounded-lg bg-[#373737]'>Web Development</h2>
-                <h3 className='mt-5'>by Dr. Kushan Sudheera</h3>
-            </div>
-        </div>
-
-        <div className='rounded-xl m-5 bg-[#d2d2d2]'>
-            <div
-                className="backImage bg-center bg-cover rounded-[50%] mt-10 mx-auto w-[170px] h-[170px]">
-            </div>
-
-            <div className='flex flex-col items-center justify-center'>
-                <h2 className='text-xl mt-10 py-1 px-5 text-white rounded-lg bg-[#373737]'>CIMA</h2>
-                <h3 className='mt-5'>by Dr. Manoj Perera</h3>
-            </div>
-        </div>
-
-        <div className='rounded-xl m-5 bg-[#d2d2d2]'>
-            <div
-                className="backImage bg-center bg-cover rounded-[50%] mt-10 mx-auto w-[170px] h-[170px]">
-            </div>
-
-            <div className='flex flex-col items-center justify-center'>
-                <h2 className='text-xl mt-10 py-1 px-5 text-white rounded-lg bg-[#373737]'>Chemistry</h2>
-                <h3 className='mt-5'>by Mr. Withanage</h3>
-            </div>
-        </div>
-
-        <div className='rounded-xl m-5 bg-[#d2d2d2]'>
-            <div
-                className="backImage bg-center bg-cover rounded-[50%] mt-10 mx-auto w-[170px] h-[170px]">
-            </div>
-
-            <div className='flex flex-col items-center justify-center'>
-                <h2 className='text-xl mt-10 py-1 px-5 text-white rounded-lg bg-[#373737]'>A/L Mathematics</h2>
-                <h3 className='mt-5'>by Mr. Ajantha Dissanayake</h3>
-            </div>
-        </div>
-        
-      </div>
-      
     </div>
-  )
+  );
 }
 
-export default Courses
+export default Courses;
--- Frontend/src/pages/StudentPage/StudentPay.jsx
+++ Frontend/src/pages/StudentPage/StudentPay.jsx
@@ -10,11 +10,10 @@ import { Link } from "react-router-dom";
 function StudentPay() {
   return (
     <div>
-          <Navbar />
-          <div className="flex ">
-
-            <div className="w-1/12 bg-[#d9d9d9] opacity-50 h-[100vh]">
-            <ol className="text-center scale-[2] mt-32">
+      <Navbar />
+      <div className="flex ">
+        <div className="w-1/12 bg-[#d9d9d9] opacity-50 h-[100vh]">
+          <ol className="text-center scale-[2] mt-32">
             <li className="py-2">
               <Link to={"/studentDashboard"}>
                 <DashboardIcon />
@@ -45,117 +44,12 @@ function StudentPay() {
               </Link>
             </li>
           </ol>
-            </div>
-
-            <div className="flex flex-col mx-[5vw] w-10/12 justify-start">
-                <div className="mt-[8vh] mb-[3vh] font-bold text-2xl">
-                    <h1>Order Details</h1>
-                </div>
-
-                <div className="flex items-center">
-
-                    <div className="scale-[0.4] -ml-[6vw]">
-                        <img src="../img/math-teacher.jpg" alt="" />
-                    </div>
-
-                    <div className="flex justify-between w-full bg-gradient-to-r from-slate-100 to-slate-300 -ml-[2vw] px-[1vw] items-center py-[2vh]">
-                        <div className="">
-                            <div className="text-lg font-bold">
-                                <h1>Matlab for Beginners</h1>
-                            </div>
-                            <div>
-                                 <h3>3 Month course-Certificate</h3>
-                            </div>  
-                        </div>
-                        <div className="text-lg font-bold">
-                         <h1>Rs. 25000</h1>
-                        </div>
-                         
-                    </div>
-
-                </div>
-
-                <div className="flex">
-                    <div className="flex flex-col w-1/2 mr-2 border border-black">
-                        <div className="pl-3 mt-2 text-lg font-bold">
-                            <h1>Payment Method</h1>
-                        </div>
-                        <div className="flex flex-col pl-3">
-                            <div className="w-[90%] my-4 border border-black p-2">
-                                <input type="radio" id="creditCard" name="creditCard" value="creditCard"/>
-                                <label for="creditCard" className="ml-2"> Credit Card</label>
-                            </div>
-                            <div className="w-[90%] my-4 border border-black p-2">
-                                <input type="radio" id="creditCard" name="creditCard" value="creditCard"/>
-                                <label for="creditCard" className="ml-2"> Debit Card</label>
-                            </div>
-                            <div className="w-[90%] my-4 border border-black p-2">
-                                <input type="radio" id="creditCard" name="creditCard" value="creditCard"/>
-                                <label for="creditCard" className="ml-2"> Bank Transfer</label>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="flex flex-col w-1/2 ml-2 border border-black bg-gradient-to-b from-slate-300 to-slate-200">
-                        <div className="pl-5 my-2 mb-6 text-lg font-bold">
-                            <h1>Summary</h1>
-                        </div>
-
-                        <div className="flex flex-col">
-                            <div className="flex px-5 mb-3 text-lg">
-                                <div className="min-w-[120px]">
-                                    <h1>Course Price</h1>
-                                </div>
-                                <div className="min-w-[200px]">
-                                  <h1>:</h1>
-                                </div>
-                                <div>
-                                    <h1>Rs. 25000</h1>
-                                </div>
-                            </div>
-
-                            <div className="flex px-5 mb-3 text-lg">
-                                <div className="min-w-[120px]">
-                                    <h1>Discounts</h1>
-                                </div>
-                                <div className="min-w-[200px]">
-                                  <h1>:</h1>
-                                </div>
-                                <div>
-                                    <h1>Rs. 200</h1>
-                                </div>
-                            </div>
-
-                            <div >
-                                <hr className="ml-5 mr-10 border border-black"/>
-                            </div>
-
-                            <div className="flex px-5 mb-8 text-lg">
-                                <div className="min-w-[120px]">
-                                    <h1>Total Price</h1>
-                                </div>
-                                <div className="min-w-[200px]">
-                                  <h1>:</h1>
-                                </div>
-                                <div>
-                                    <h1>Rs. 24800</h1>
-                                </div>
-                            </div>
-
-                        </div>
-
-                        <div className="flex justify-center">
-                            <input type="submit" value="Proceed" className="px-10 py-2 text-white bg-orange-500 rounded-xl border-[1px] border-orange-500 hover:bg-white hover:text-orange-500 cursor-pointer tracking-wider text-xl uppercase"/>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-
         </div>
-    </div>  
-       
-  )
+
+        <div className="flex flex-col mx-[5vw] w-10/12 justify-start"></div>
+      </div>
+    </div>
+  );
 }
 
-export default StudentPay
+export default StudentPay;
